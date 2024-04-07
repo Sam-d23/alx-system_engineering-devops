7803904b794c13a082c440c85e243514a3eee2c6
Sam-d23 <samkiragu05@gmail.com>
2024-04-07 13:57:21 -0700

    Task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,32 +1,34 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
+"""To-do list information for a given employee ID is exported to CSV."""
+
 import csv
 import requests
 import sys
 
-if __name__ == "__main__":
-    # Check if the correct number of arguments is provided
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
 
-    employee_id = sys.argv[1]
+if __name__ == "__main__":
+    #The user ID is gotten from the command-line arguments
+    user_id = sys.argv[1]
+
+    #The base URL for the JSON API is defined
     url = "https://jsonplaceholder.typicode.com/"
 
-    # Fetch user information
-    user_response = requests.get(url + "users/{}".format(employee_id))
-    user_data = user_response.json()
-    username = user_data.get("username")
+    #user information is fetched from the API and
+    #The response is converted to a JSON object
+    user = requests.get(url + "users/{}".format(user_id)).json()
 
-    # Fetch TODO list for the employee
-    todos_response = requests.get(url + "todos",
-                                  params={"userId": employee_id})
-    todos_data = todos_response.json()
+    #The username is extracted from the user data
+    username = user.get("username")
 
-    # Write TODO list information to CSV file
-    with open("{}.csv".format(employee_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(["Employee ID", "Username", "Completed", "Title"])
-        for todo in todos_data:
-            writer.writerow([employee_id, username, todo["completed"],
-                             todo["title"]])
+    #the to-do list items associated with the given
+    #user ID and convert the response to a JSON object are fetched
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    # list comprehension is used to iterate over the to-do list items
+    # Each item's details is written (user ID, username, completion status,
+    #   and title) as a row in the CSV file
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
