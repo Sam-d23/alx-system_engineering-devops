35444d57c717e3b893b5a079e673a9971d3d2f35
Sam-d23 <samkiragu05@gmail.com>
2024-04-09 10:16:03 -0700

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,22 +5,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     """The total number of subscribers on a given subreddit is returned."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 404:
-            return 0
-        data = response.json().get("data")
-        return data.get("subscribers")
-    except requests.RequestException as e:
-        print("Error:", e)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-
-
-# Example of usage:
-subreddit = "python"
-print(f"The number of subscribers in r/{subreddit} is:",
-      number_of_subscribers(subreddit))
+    results = response.json().get("data")
+    return results.get("subscribers")
