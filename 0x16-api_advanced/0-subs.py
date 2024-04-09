b6ece90a1860f7cf292c23ce0e8a2d9f8bc16ffd
Sam-d23 <samkiragu05@gmail.com>
2024-04-09 09:34:48 -0700

    Task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import requests
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "MyRedditBot/1.0"}  # Custom User-Agent
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException as e:
+        print("Error:", e)
+        return 0
+
+# Example usage:
+subreddit = "python"
+print(f"The number of subscribers in r/{subreddit} is:",
+      number_of_subscribers(subreddit))
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,1 @@
+Project tasks for the 0x16. API advanced project
