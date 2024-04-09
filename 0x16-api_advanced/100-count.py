bb045efc02a0dac2b1113fe145c2fac55cd61b1a
Sam-d23 <samkiragu05@gmail.com>
2024-04-09 11:19:19 -0700

    Task 3

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Counts words in all hot posts of a given Reddit subreddit."""
+import requests
+
+
+def count_words(subreddit, word_list, instances={}, after="", count=0):
+    """Counts of given words found in hot posts of a given subreddit are printed.
+
+    Args:
+        subreddit (str): subreddit being search.
+        word_list (list): list of words to found in post titles.
+        instances (obj): Key/value pairs of words/counts.
+        after (str): indicates the next page of the API results.
+        count (int): indicates the results matched thus far.
+    """
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    try:
+        results = response.json()
+        if response.status_code == 404:
+            raise Exception
+    except Exception:
+        print("")
+        return
+
+    results = results.get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        title = c.get("data").get("title").lower().split()
+        for word in word_list:
+            if word.lower() in title:
+                times = len([t for t in title if t == word.lower()])
+                if instances.get(word) is None:
+                    instances[word] = times
+                else:
+                    instances[word] += times
+
+    if after is None:
+        if len(instances) == 0:
+            print("")
+            return
+        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
+        [print("{}: {}".format(k, v)) for k, v in instances]
+    else:
+        count_words(subreddit, word_list, instances, after, count)
