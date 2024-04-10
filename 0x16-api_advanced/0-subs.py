42cc59eb4b0c48650f7d9d6959ce8efc1879695d
Sam-d23 <samkiragu05@gmail.com>
2024-04-10 03:15:59 -0700

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,13 +4,20 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """The total number of subscribers on a given subreddit is returned."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Custom User Agent'}
+
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+
+
+# Example usage:
+subreddit = "python"
+print(f"Number of subscribers in r/{subreddit}:
+      {number_of_subscribers(subreddit)}")
