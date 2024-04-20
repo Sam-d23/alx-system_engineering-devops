3132ae6910ce40ab5a34ad1513940c34058c568a
Sam-d23 <samkiragu05@gmail.com>
2024-04-21 00:31:05 +0300

    Task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# This script comments out the ulimit line in /etc/default/nginx
+
+exec { 'fix nginx ulimit':
+  command => '/bin/sed -i "s/ULIMIT/# ULIMIT/" /etc/default/nginx; /usr/sbin/service nginx restart'
+}
---
+++ 0x1B-web_stack_debugging_4/README.md
@@ -0,0 +1,1 @@
+Project tasks for 0x1B-web_stack_debugging_4
